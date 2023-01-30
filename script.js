9006ea01f41506720393c3db1537fd05d170d0d2
yashPundhir <yash.pundhir.prof@gmail.com>
2023-01-30 17:14:09 +0530

    Next & Prev functionality added to buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let playBtn = document.querySelector("#play");
 let pauseBtn = document.querySelector("#pause");
 let nextBtn = document.querySelector("#next");
+let prevBtn = document.querySelector("#prev");
 let video = document.querySelector(".video-file");
 function videoPlay() {
 	playBtn.style.display = "none";
@@ -15,4 +16,18 @@ function videoPause() {
 	playBtn.style.marginLeft = "0px";
 	nextBtn.style.marginLeft = "0px";
 	video.pause();
+}
+function videoNext() {
+	nextBtn.style.opacity = "0.3";
+	prevBtn.style.opacity = "1";
+	video.src = "./Videos/Markdown syntax Cheat sheet.mp4";
+	pauseBtn.style.display = "none";
+	playBtn.style.display = "inline";
+}
+function videoPrev() {
+	prevBtn.style.opacity = "0.3";
+	nextBtn.style.opacity = "1";
+	video.src = "./Videos/Ant-Man and The Wasp Quantumania.mp4";
+	pauseBtn.style.display = "none";
+	playBtn.style.display = "inline";
 }
